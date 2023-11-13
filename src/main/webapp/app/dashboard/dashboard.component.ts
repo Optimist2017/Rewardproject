1aedd1d5935e38dd4cee204cb14f8fba685ec3e7
Mohammed Dayyan <57920240+MohammedDayan@users.noreply.github.com>
2023-11-13 23:25:45 +0000

    tasks views added

--- src/main/webapp/app/dashboard/dashboard.component.ts
+++ src/main/webapp/app/dashboard/dashboard.component.ts
@@ -11,6 +11,22 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit, OnDestroy {
+  items = [
+    { title: 'Exploring Angular 12 Features', content: 'In this video, we explore the latest features in Angular 12.', link: 'https://www.youtube.com/watch?v=pq34V_V5j18', likes: 1200 },
+    { title: 'React State Management Techniques', content: 'Learn different state management techniques in React.', link: 'https://www.youtube.com/watch?v=abc', likes: 800 },
+    { title: 'Vue.js Crash Course for Beginners', content: 'A beginner-friendly crash course on Vue.js framework.', link: 'https://www.youtube.com/watch?v=pqr', likes: 1500 },
+    { title: 'Building RESTful APIs with Spring Boot', content: 'Create RESTful APIs using Spring Boot and Java.', link: 'https://www.youtube.com/watch?v=lmn', likes: 2000 },
+    { title: 'Python Web Scraping Tutorial', content: 'Learn how to scrape websites using Python and BeautifulSoup.', link: 'https://www.youtube.com/watch?v=def', likes: 600 },
+    { title: 'Machine Learning Basics with Scikit-Learn', content: 'Introduction to machine learning using Scikit-Learn in Python.', link: 'https://www.youtube.com/watch?v=jkl', likes: 1300 },
+    // Add more items as needed
+  ];
+
+  itemd = [
+    { title: 'CSS Grid Layout Tutorial', content: 'Master the CSS Grid layout for building responsive web designs.', link: 'https://www.youtube.com/watch?v=mno', likes: 1100 },
+    { title: 'Dockerize Your Node.js App', content: 'Containerize your Node.js application with Docker for deployment.', link: 'https://www.youtube.com/watch?v=ghi', likes: 900 },
+
+  ];
+  activeTab = 'new-tasks';
   account: Account | null = null;
   private readonly destroy$ = new Subject<void>();
   constructor(private router: Router, private accountService: AccountService) {}
@@ -28,12 +44,24 @@ export class DashboardComponent implements OnInit, OnDestroy {
     });
   }
 
-  watch(): void {
-    this.router.navigate(['/dashboard/watch']);
+
+
+  showTab(tab: string): void {
+    this.activeTab = tab;
+  }
+
+  watch(selectedItem:any): void {
+    const navigationExtras = {
+      state: {
+        youtubeUrl: selectedItem.link
+      },
+    };
+    this.router.navigate(['/dashboard/watch'],navigationExtras);
   }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
+
 }
--- src/main/webapp/app/watch/watch.component.ts
+++ src/main/webapp/app/watch/watch.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 @Component({
   selector: 'jhi-watch',
   templateUrl: './watch.component.html',
   styleUrls: ['./watch.component.scss'],
 })
-export class WatchComponent {}
+export class WatchComponent  implements OnInit {
+  youtubeUrl: string | any;
+
+  constructor(private route: ActivatedRoute) {}
+  
+
+    ngOnInit(): void {
+      // Subscribe to route changes to get the latest state
+      this.route.paramMap.pipe(take(1)).subscribe((params) => {
+        // Retrieve the data from the route
+        this.youtubeUrl = params.get('youtubeUrl');
+
+      });
+     
+  }
+  
+}
